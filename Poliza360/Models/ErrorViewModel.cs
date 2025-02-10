26499e067ae5f2a48a3b723bce78f30872ba50a5
DELL <DELL@TERACROM13>
2025-02-10 09:59:19 -0600

    Agregar archivos de proyecto.

---
+++ Poliza360/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Poliza360.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
